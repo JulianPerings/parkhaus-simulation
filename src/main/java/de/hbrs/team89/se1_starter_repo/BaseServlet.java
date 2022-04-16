f60c71f3c3f15faaacfed927ebf4baabf7cbb5b4
Christopher Falke <cfalke2s@smail.inf.h-brs.de>
2022-04-16 18:37:19 +0200

    Added sum button

---
+++ src/main/java/de/hbrs/team89/se1_starter_repo/BaseServlet.java
@@ -0,0 +1,21 @@
+package de.hbrs.team89.se1_starter_repo;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name ="Super Duper Parkhaus" ,value= "/BaseServlet")
+public class BaseServlet extends ParkhausServlet {
+    @Override
+    String NAME() {
+        return "Super Duper Parkhaus";
+    }
+    double sum = 0;
+    @Override
+    int MAX() {
+        return 0;
+    }
+
+    @Override
+    String config() {
+        return null;
+    }
+}
