913a0cb226a5003d87578e297d90eb28747e6945
akrink2s <alexander.krinke@smail.inf.h-brs.de>
2022-05-06 22:27:22 +0200

    added getPriced GET Method that prints day and night prices

--- src/test/java/de/hbrs/team89/se1_starter_repo/PriceCalcTest.java
+++ src/test/java/de/hbrs/team89/se1_starter_repo/PriceCalcTest.java
@@ -8,11 +8,12 @@ class PriceCalcTest {
     PriceCalc testCalc = new PriceCalc();
     @Test
     void calcDayNightPrice_methodCorrect_expectedNear8() {
-        int duration=1000*60*60*5;
-        long begin=0;
-        double price=10;
-        double expectedPrice=10*0.8;
+        int duration=1000*60*60*5;  //5 Stunden
+        long begin=0;   //Startzeitpunkt
+        double price=10;    //Preis?
+        double expectedPrice=10*0.8;    //soll 8 euro/cent? kosten
         assertEquals(expectedPrice,testCalc.calcDayNightPrice(price,begin,duration),0.01);
+        assertEquals(expectedPrice,testCalc.calcDayNightPrice(begin,duration),0.01);
     }
     @Test
     void calcDayNightPrice_wrongInput_expected0(){
