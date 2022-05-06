d54823eaf81a5baa749a9962af590763d7a8c010
akrink2s <alexander.krinke@smail.inf.h-brs.de>
2022-05-06 22:28:47 +0200

    added method descriptions

--- src/main/java/de/hbrs/team89/se1_starter_repo/ParkingGarage.java
+++ src/main/java/de/hbrs/team89/se1_starter_repo/ParkingGarage.java
@@ -2,22 +2,26 @@ package de.hbrs.team89.se1_starter_repo;
 
 public class ParkingGarage implements ParkingGarageIF {
     ParkingLot[] spaces;
+    /**Initialize ParkingGarage with m parking lots which only allow PKW's.*/
     public ParkingGarage(int m){
         spaces = new ParkingLot[m];
         for(int i = 0; i < spaces.length;i++){
             spaces[i] = new ParkingLot(new String[]{"PKW"});
         }
     }
+    /**Returns clone of garage.*/
     @Override
     public ParkingLotIF[] getGarage() {
         return spaces.clone();
     }
 
+    /**Returns reference to original garage so that garage can be changed.*/
     @Override
     public ParkingLotIF[] getGarageUnprotected() {
         return spaces;
     }
 
+    /**Parks car in garage, returns true if parked successful and false if there was no fitting parking spot.  */
     @Override
     //without priority atm
     public boolean parkCar(CarIF c) {
@@ -30,6 +34,9 @@ public class ParkingGarage implements ParkingGarageIF {
         return false;
     }
 
+    /**Takes string array of vehicle types and returns int array of free parking lots for given types.
+     * Example: getFreeParkingSpaces({"PKW", "E_VEHICLE"}) returns {10, 7} would mean that there are 10 lots where a PKW can park
+     * and 7 lots suitable for E_VEHICLEs.*/
     @Override
     public int[] getFreeParkingSpaces(String[] s) {
         if(s != null) {
@@ -50,6 +57,8 @@ public class ParkingGarage implements ParkingGarageIF {
         }
     }
 
+    /**Takes string array of vehicle types and returns int array of total parking lots for given types.
+     * Example: getFreeParkingSpaces({"PKW", "E_VEHICLE"}) returns {10, 7} would mean that there is a total of 10 PKS lots and 7 E_VEHICLE lots.*/
     @Override
     public int[] getParkingSpaces(String[] s) {
         if(s != null) {
@@ -68,6 +77,7 @@ public class ParkingGarage implements ParkingGarageIF {
         }
     }
 
+    /**Removes and returns given car from the garage.*/
     @Override
     public CarIF removeCar(CarIF c) {
         for(ParkingLot p : spaces){
