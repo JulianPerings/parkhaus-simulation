2ed3e04eab96545a4abbf200c331b90b18f15c31
jperin2s <Jul-Per+2002>
2022-06-19 19:04:23 +0200

    added Vehicletypes tests

---
+++ src/test/java/de/hbrs/team13/parkhaus_team13/Singleton/VehicleTypesTest.java
@@ -0,0 +1,41 @@
+package de.hbrs.team13.parkhaus_team13.Singleton;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTypesTest {
+    VehicleTypes a,c;
+    @BeforeEach
+    void setUp() {
+        a= new VehicleTypes("MOTORBIKE",0.8,0.5);
+        c= new VehicleTypes("",0,0);
+    }
+
+    @Test
+    void getInstance() {
+        VehicleTypes b = VehicleTypes.getInstance();
+        assertEquals(VehicleTypes.instance,b);
+    }
+
+    @Test
+    void getVehicleType() {
+        assertEquals("MOTORBIKE",a.getVehicleType());
+    }
+
+    @Test
+    void getPriceFactor() {
+        assertEquals(0.8,a.getPriceFactor());
+    }
+
+    @Test
+    void getReqSpace() {
+        assertEquals(0.5,a.getReqSpace());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("Type : MOTORBIKE",a.toString());
+    }
+}
