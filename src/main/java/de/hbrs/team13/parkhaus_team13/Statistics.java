2ae3a4332b10a0cbfb092bc45ce3c638841698a9
akrink2s <alexander.krinke@smail.inf.h-brs.de>
2022-06-20 19:50:34 +0200

    Added comments.

--- src/main/java/de/hbrs/team13/parkhaus_team13/Statistics.java
+++ src/main/java/de/hbrs/team13/parkhaus_team13/Statistics.java
@@ -18,34 +18,42 @@ public class Statistics extends Observable {
         return cars;
     }
 
-    /**Returns the sum of all prices in cents.*/
+    /**@return The price paid overall in cents.*/
     double getSum() {
         return cars.stream().map(Car::getPrice).reduce( 0., (x, y) -> x + y );
     }
 
-    /**Return the average price in cents cut after 2 decimal places.  */
+    /**@return The average price in cents cut after 2 decimal places.  */
     double getAvg() {
         return cars.size() == 0 ? 0 : Math.floor((getSum() / cars.size()) * 100) / 100;
     }
 
+    /**@return The time parked overall in seconds.*/
     double getTime() {
         return cars.stream().map(Car::getDuration).reduce( 0, (x, y) -> x + y );
     }
 
+    /**@return The average time parked in seconds. */
     int getAvgTime() {
         return cars.size() == 0 ? 0 : (int) (getTime() / cars.size());
     }
 
+    /**@return Array with VehicleTypes*/
     String[] getVehicleTypes() {
         return vehicleTypes;
     }
 
+    /**@return Array with ClientCategories*/
     String[] getClientCategory() {
         return clientCategory;
     }
 
 
-
+    /**
+     * Returns array of Integers representing the number of each VehicleType.
+     * Use getVehicleTypes to see the VehicleType each Integer belongs to.
+     * @return Array of Integers
+     * */
     int[] countVehicleType() {
         int[] counterVehicleTypes = {0, 0, 0, 0};
 
@@ -57,11 +65,15 @@ public class Statistics extends Observable {
         return counterVehicleTypes;
     }
 
+    /**
+     * @return Car that has parked over the shortest period of time*/
     Car getMin(){
         Comparator<Car> c = (s1, s2) -> s1.getDuration() - s2.getDuration();
         return cars.stream().reduce((c1, c2) -> c.compare(c1, c2) <= 0 ? c1 : c2).orElse(null);
     }
 
+    /**
+     * @return Car that has parked over the longest period of time*/
     Car getMax(){
         Comparator<Car> c = (s1, s2) -> s1.getDuration() - s2.getDuration();
         return cars.stream().reduce((c1, c2) -> c.compare(c1, c2) >= 0 ? c1 : c2).orElse(null);
