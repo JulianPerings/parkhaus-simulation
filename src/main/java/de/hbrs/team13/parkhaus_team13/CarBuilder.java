255141ec24d86bf53605f6416a3350f993f71244
akrink2s <alexander.krinke@smail.inf.h-brs.de>
2022-06-25 17:55:05 +0200

    Implemented CarBuilder to make building cars for e.g. testing, sanitizing easier

---
+++ src/main/java/de/hbrs/team13/parkhaus_team13/CarBuilder.java
@@ -0,0 +1,67 @@
+package de.hbrs.team13.parkhaus_team13;
+
+public class CarBuilder implements CarBuilderIF{
+    String[] params;
+
+    /**
+     * Builds a car. Single Parameters can be set using the Methods, otherwise they will be random.
+     */
+    public CarBuilder(){
+        params = new Car().getParams();
+    }
+
+    @Override
+    public void buildNr(int nr) {
+        params[0] = "\"nr\": " + nr;
+    }
+
+    @Override
+    public void buildTimer(long timer) {
+        params[1] = "\"timer\": " + timer;
+    }
+
+    @Override
+    public void buildDuration(int duration) {
+        params[2] = "\"duration\": " + duration;
+    }
+
+    @Override
+    public void buildPrice(int price) {
+        params[3] = "\"price\": " + price;
+    }
+
+    @Override
+    public void buildHash(String hash) {
+        params[4] = "\"hash\": \"" + hash + "\"";
+    }
+
+    @Override
+    public void buildColour(String colour) {
+        params[5] = "\"color\": \"" +colour + "\"";
+    }
+
+    @Override
+    public void buildSpace(int space) {
+        params[6] = "\"space\": " + space;
+    }
+
+    @Override
+    public void buildClient_Category(String clientCat) {
+        params[7] = "\"client_category\": \"" + clientCat + "\"";
+    }
+
+    @Override
+    public void buildVehicle_type(String VehicleType) {
+        params[8] = "\"vehicle_type\": \"" + VehicleType + "\"";
+    }
+
+    @Override
+    public void buildLicense(String license) {
+        params[9] = "\"license\": \"" + license + "\"";
+    }
+
+    @Override
+    public Car buildCar() {
+        return new Car(params);
+    }
+}
---
+++ src/main/java/de/hbrs/team13/parkhaus_team13/CarBuilderIF.java
@@ -0,0 +1,16 @@
+package de.hbrs.team13.parkhaus_team13;
+
+public interface CarBuilderIF {
+    public void buildNr(int nr);
+    public void buildTimer(long timer);
+    public void buildDuration(int duration);
+    public void buildPrice(int price);
+    public void buildHash(String hash);
+    public void buildColour(String colour);
+    public void buildSpace(int space);
+    public void buildClient_Category(String clientCat);
+    public void buildVehicle_type(String VehicleType);
+    public void buildLicense(String license);
+
+    public Car buildCar();
+}
